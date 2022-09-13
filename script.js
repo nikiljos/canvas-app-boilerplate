ae5f471207b63c89fea3813a3cf3eea983c76a7f
Nikhil Jose <nikhiljose12345@gmail.com>
2022-09-13 16:59:38 +0530

    feat: added color buttons, color change script

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 /* Iteration 4: Make the Everything Work */
+const components = ["sky", "ground", "body", "ears", "cheeks"];
+
+function changeColor(id) {
+    let elt = document.getElementById(id);
+    let color = document.getElementById(`btn-${id}`);
+    elt.style.fill = color.value;
+}
+
+function clearColor() {
+    components.forEach((id) => {
+        let elt = document.getElementById(id);
+        let color = document.getElementById(`btn-${id}`);
+        elt.style.fill = "#ffffff";
+        color.value = "#ffffff";
+    });
+}
+
+function autoColor() {
+    components.forEach((id) => {
+        let randomColor = `#${Math.floor(Math.random() * 16777215).toString(
+            16
+        )}`;
+        let elt = document.getElementById(id);
+        let color = document.getElementById(`btn-${id}`);
+        elt.style.fill = randomColor;
+        color.value = randomColor;
+    });
+}
